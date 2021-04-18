388078b84688db312d46a7fc1a8e0f14a8f10b59
Somi47 <somorjai.balazs@danubisoft.hu>
2021-04-18 15:35:51 +0200

    Api mock objects added

---
+++ app/app/src/main/java/com/example/android2jobapp/client/api/mock/MockApplicationApi.kt
@@ -0,0 +1,27 @@
+package com.example.android2jobapp.client.api.mock
+
+import com.example.android2jobapp.client.api.ApplicationApi
+import com.example.android2jobapp.model.Application
+import retrofit2.Call
+
+class MockApplicationApi : ApplicationApi {
+    override fun getApplications(): Call<Array<Application>?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getApplicationId(id: String?): Call<Application?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun postApplication(body: Application?): Call<Void?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun putApplicationId(id: String?, body: Application?): Call<Void?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteApplicationId(id: String?): Call<Void?>? {
+        TODO("Not yet implemented")
+    }
+}
